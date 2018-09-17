b6388713b2333e8199bc816665d76111ad32a097
Jordan Russell <jordanrussell1@gmail.com>
2018-09-17 18:55:14 -0400

    seeds complete

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ("Melted Swiss Cheese and Shrooms Burger");
+INSERT INTO burgers (name) VALUES ("Buffalo Bleu Cheese Burger");
+INSERT INTO burgers (name) VALUES ("Bacon Cheddar Burger");
