b2743b0695c9ab68d6dfe8c29b009225b03ad06b
Jordan Russell <jordanrussell1@gmail.com>
2018-09-17 19:05:53 -0400

    burger.js done

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+//ORM import
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    //Variable cols and vals are arrays
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+    orm.update("cats", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+//export db functions for the controller
+module.exports = burger;
